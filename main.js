161a9f2041b02a6aa272c165f4e5a9871226b14b
Sanaa <sanaa@pop-os.localdomain>
2020-11-26 15:19:18 +0100

    all

---
+++ main.js
@@ -0,0 +1,13 @@
+mediumZoom(.zoom,{
+    margin : 50,
+    scrollOffset: 200, 
+});
+mediumZoom(.zoom-dark,{
+   background: #000 
+});
+mediumZoom(.zoom-tmp, {
+    background: #BADA55,
+    container: #zoom-container,
+    template: #zoom-template,
+  })
+  
